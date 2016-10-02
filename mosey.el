b0ea446e4e0d45770a9c49345d2269e5ad190ca2
Adam Porter <adam@alphapapa.net>
2016-10-02 01:54:45 -0500

    s/position-funcs/move-funcs/g
    Clear as mud

--- mosey.el
+++ mosey.el
@@ -60,14 +60,17 @@
 
 ;;;; Mosey function and macro
 
-(cl-defun mosey (position-funcs &key (backward nil backward-set) (cycle nil cycle-set))
-  "Move the point according to the list of POSITION-FUNCS.
+(cl-defun mosey (move-funcs &key (backward nil backward-set) (cycle nil cycle-set))
+  "Move the point according to the list of MOVE-FUNCS.
+
+Each function in MOVE-FUNCS should move the point to a
+significant position, usually on the current line, but
+potentially across lines.
 
 If BACKWARD is set, move backwards.
 
 If CYCLE is set, cycle around when the beginning/end of line is
 hit.  Otherwise, stop at beginning/end of line."
-  ;; position-funcs should MOVE THE POINT, not just return the position
   (interactive)
   (let* ((backward backward-set)
          (cycle cycle-set)
@@ -77,7 +80,7 @@ hit.  Otherwise, stop at beginning/end of line."
          (positions (sort (delete-dups (mapcar (lambda (func) (save-excursion
                                                                 (funcall func)
                                                                 (point)))
-                                               position-funcs))
+                                               move-funcs))
                           '<))
          ;; Reverse list if :backward is set (is there a more elegant way to do this?)
          (positions (if backward
@@ -93,11 +96,13 @@ hit.  Otherwise, stop at beginning/end of line."
     ;; Goto the target position
     (goto-char target)))
 
-(cl-defmacro defmosey (position-funcs &key prefix)
+(cl-defmacro defmosey (move-funcs &key prefix)
   "Define `mosey/forward' and `mosey/backward' functions, with
 `-cycle' variants.
 
-POSITION-FUNCS is a list of functions that move the point.
+MOVE-FUNCS is a list of functions that should should move the
+point to a significant position, usually on the current line, but
+potentially across lines.
 
 PREFIX, if set, appends a prefix to the function names, like
 `mosey/prefix-forward', useful for defining different sets of
@@ -106,19 +111,19 @@ moseys for different modes."
     (setq prefix (concat prefix "-")))
   `(progn (defun ,(intern (concat "mosey/" prefix "forward")) ()
             (interactive)
-            (mosey ,position-funcs)
+            (mosey ,move-funcs)
             )
           (defun ,(intern (concat "mosey/" prefix "backward")) ()
             (interactive)
-            (mosey ,position-funcs :backward)
+            (mosey ,move-funcs :backward)
             )
           (defun ,(intern (concat "mosey/" prefix "forward-cycle")) ()
             (interactive)
-            (mosey ,position-funcs :cycle)
+            (mosey ,move-funcs :cycle)
             )
           (defun ,(intern (concat "mosey/" prefix "backward-cycle")) ()
             (interactive)
-            (mosey ,position-funcs :backward :cycle))))
+            (mosey ,move-funcs :backward :cycle))))
 
 ;;;; Helper functions
 
